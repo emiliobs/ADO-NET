5080d7e0f13073eec36988584a4f565fcd789842
emilio barrera <barrera_emilio@hotmail.com>
2016-03-28 01:00:06 +0200

    sqlbulkcopynotifyafterexample, xmlcopy

---
+++ UtilidadesADO.NET/Loadxmldataintosqlservertableusingsqlbulkcopy/index.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+namespace Loadxmldataintosqlservertableusingsqlbulkcopy
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(Server.MapPath("~/Data.xml"));
+
+                DataTable dtDepartment = ds.Tables["department"];
+                DataTable dtEmployee = ds.Tables["Employee"];
+
+                con.Open();
+
+                using (SqlBulkCopy bc = new SqlBulkCopy(con))
+                {
+                    bc.DestinationTableName = "Department";
+                    bc.ColumnMappings.Add("id", "DepartmentId");
+                    bc.ColumnMappings.Add("name", "Name");
+                    bc.ColumnMappings.Add("location", "Location");
+                    bc.WriteToServer(dtDepartment);
+                }
+
+
+                using (SqlBulkCopy bc = new SqlBulkCopy(con))
+                {
+                    bc.DestinationTableName = "tblEmployees";
+                    bc.ColumnMappings.Add("Id", "EmployeeId");
+                    bc.ColumnMappings.Add("Name", "Name");
+                    bc.ColumnMappings.Add("Gender", "Gender");
+                    bc.ColumnMappings.Add("salary", "Salary");
+                    bc.ColumnMappings.Add("DepartmentId", "DepartmentId");
+                    bc.WriteToServer(dtEmployee);
+                }
+
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string csSource = ConfigurationManager.ConnectionStrings["SourceDBCS"].ConnectionString;
+            string csDestination = ConfigurationManager.ConnectionStrings["DestinationDBCS"].ConnectionString;
+
+            using (SqlConnection consource = new SqlConnection(csSource))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Department", consource);
+                consource.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    using (SqlConnection conDestination = new SqlConnection(csDestination))
+                    {
+                        using (SqlBulkCopy bc = new SqlBulkCopy(conDestination))
+                        {
+                            bc.DestinationTableName = "Department";
+                            conDestination.Open();
+                            bc.WriteToServer(dr);
+                        }
+                    }
+                }
+
+                    cmd = new SqlCommand("Select * from tblEmployees", consource);
+                    //consource.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        using (SqlConnection conDestination = new SqlConnection(csDestination))
+                        {
+                            using (SqlBulkCopy bc = new SqlBulkCopy(conDestination))
+                            {
+                                bc.DestinationTableName = "tblEmployees";
+                                conDestination.Open();
+                                bc.WriteToServer(dr);
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    
---
+++ UtilidadesADO.NET/sqlbulkcopynotifyafterexample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace sqlbulkcopynotifyafterexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection conSource = new SqlConnection (cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from ProductsSource", conSource);
+                conSource.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    using (SqlConnection conDestination = new SqlConnection(cs))
+                    {
+                        using (SqlBulkCopy bc = new SqlBulkCopy (conDestination))
+                        {
+                            bc.BatchSize = 10000;
+                            bc.NotifyAfter = 5000;
+                            /* bc.SqlRowsCopied += new SqlRowsCopiedEventHandler(bc_SqlRowsCopied);  */
+                            bc.SqlRowsCopied += (sender, EventArgs) =>
+                            {
+                                Console.WriteLine(EventArgs.RowsCopied + " Loaded..... ");
+                            };
+
+                            bc.DestinationTableName = "ProductsDestination";
+                            conDestination.Open();
+                            bc.WriteToServer(dr); 
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+    //    private static void bc_SqlRowsCopied(object sender, SqlRowsCopiedEventArgs e)
+    //    {
+    //        Console.WriteLine(e.RowsCopied + " Loaded..... ");
+
+
+
+    //        //Console.ReadKey();
+    //    }
+    //}
+}
