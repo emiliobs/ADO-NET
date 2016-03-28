b4342dae7d2344165e3943cafc4e78e277161dc8
emilio barrera <barrera_emilio@hotmail.com>
2016-03-28 12:59:57 +0200

    TransactionsinADONET

---
+++ UtilidadesADO.NET/TransactionsinADONET1/index.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="TransactionsinADONET1.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            color: #FFFFFF;
+        }
+        .auto-style3 {
+            color: #FFFFFF;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server" style="background-color:orangered">
+    <div>
+    
+        <table class="auto-style1" border="1">
+            <tr>
+                <td class="auto-style3">Account Number:</td>
+                <td>
+                    <asp:Label ID="lblAccountNumber" runat="server" CssClass="auto-style2"></asp:Label>
+                </td>
+                <td>
+                    <asp:Label ID="lblAccountNumber2" runat="server" CssClass="auto-style2"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Customer Name:</td>
+                <td>
+                    <asp:Label ID="lblName" runat="server" CssClass="auto-style2"></asp:Label>
+                </td>
+                <td>
+                    <asp:Label ID="lblName2" runat="server" CssClass="auto-style2"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Balance:</td>
+                <td>
+                    <asp:Label ID="lblbalance" runat="server" CssClass="auto-style2"></asp:Label>
+                </td>
+                <td>
+                    <asp:Label ID="lblBalance1" runat="server" CssClass="auto-style2"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <asp:Button ID="btnTransfer" runat="server" Text="Tranfer $10 from Account A1 to Account A2." OnClick="btnTransfer_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3" class="auto-style3">
+                    <asp:Label ID="lblMensaje" runat="server" CssClass="auto-style3"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ UtilidadesADO.NET/TransactionsinADONET1/index.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace TransactionsinADONET1
+{
+    public partial class index : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetDate();
+            }
+        }
+
+        private void GetDate()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection (cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Accounts", con);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while(dr.Read())
+                {
+                    if (dr["AccountNumber"].ToString() == "A1")
+                    {
+                        lblAccountNumber.Text = "A1";
+                        lblbalance.Text = dr["Balance"].ToString();
+                        lblName.Text = dr["CustomerName"].ToString();
+                    }
+                    else
+                    {
+                        lblAccountNumber2.Text = "A2";
+                        lblBalance1.Text = dr["Balance"].ToString();
+                        lblName2.Text = dr["CustomerName"].ToString();
+                    }
+                }
+            }
+        }
+
+        protected void btnTransfer_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection (cs))
+            {
+                con.Open();
+                SqlTransaction transaction = con.BeginTransaction();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("update Accounts set Balance = Balance - 10 where AccountNumber = 'A1' ", con,transaction);
+                   
+                    cmd.ExecuteNonQuery();
+
+                    cmd = new SqlCommand("update Accounts set Balance = Balance + 10 where AccountNumber = 'A2'", con, transaction);
+                    
+                    cmd.ExecuteNonQuery();
+
+                    transaction.Commit();
+
+                    lblMensaje.Text = "Transaction Successful.";
+                    lblMensaje.ForeColor = System.Drawing.Color.Green;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    lblMensaje.Text = "Transaction Failed.";
+                    lblMensaje.ForeColor = System.Drawing.Color.Black;
+                    //lblMensaje.Text = "ERROR: " + ex.Message;
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+                GetDate();
+            }
+        }
+    }
+}
